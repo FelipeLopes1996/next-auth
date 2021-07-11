95141b14c4dc207cd01469006a0004d57bf3417e
Felipe de Lima Lopes <felipe.lopes@pampa.compasso>
2021-07-11 14:43:36 -0300

    Initial commit from Create Next App

---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+export default {
+  // url: 'http://localhost:1337/pages/?slug=',
+  url: 'https://api-teste-landing.herokuapp.com/pages/?slug=',
+  siteName: 'Felipe de Lima',
+  defaultSlug: 'landing-page',
+};
